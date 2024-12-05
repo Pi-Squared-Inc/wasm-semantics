e66b22f16aebadb3fffd10f2a08a07740e5d0a16
Virgil <virgil.serbanuta@runtimeverification.com>
2024-12-05 19:21:26 +0200

    Extract and mock the ULM api

--- tests/ulm/erc20/rust/src/storage.rs
+++ tests/ulm/erc20/rust/src/storage.rs
@@ -8,7 +8,7 @@
 // * T must implement TryFrom<U256> and Into<U256>
 //
 // Then you can build the storage object like this:
-// 
+//
 // let mut builder = SingleChunkStorageBuilder::<MyValueType>::new(&("Storage name".to_string()))
 // builder.add_arg(K1);
 // builder.add_arg(K2);
@@ -35,46 +35,61 @@ use crate::unsigned::U256;
 use crate::ulm;
 use crate::ulm_hooks;
 
-pub struct SingleChunkStorage<ValueType:Into<U256> + TryFrom<U256> + 'static> {
-    phantom: PhantomData<ValueType>,
+pub struct SingleChunkStorage<'a, ValueType>
+    where
+        ValueType: Into<U256> + TryFrom<U256>,
+{
+    phantom_value: PhantomData<&'a ValueType>,
+    api: &'a mut dyn ulm::Ulm,
     fingerprint: U256,
 }
 
-impl<ValueType:Into<U256> + TryFrom<U256, Error = &'static str>> SingleChunkStorage<ValueType> {
-    pub fn new(fingerprint: U256) -> Self {
-        SingleChunkStorage::<ValueType> { phantom: PhantomData, fingerprint }
+impl<'a, ValueType> SingleChunkStorage<'a, ValueType>
+    where
+        ValueType: Into<U256> + TryFrom<U256, Error = &'static str>,
+{
+    pub fn new(api: &'a mut dyn ulm::Ulm, fingerprint: U256) -> Self {
+        SingleChunkStorage::<ValueType> { phantom_value: PhantomData, api, fingerprint }
     }
 
-    pub fn set(&self, value: ValueType) {
+    pub fn set(&mut self, value: ValueType) {
         let converted: U256 = value.into();
-        ulm::set_account_storage(&self.fingerprint, &converted);
+        ulm::set_account_storage(self.api, &self.fingerprint, &converted);
     }
 
     pub fn get(&self) -> ValueType {
-        let u256 = ulm::get_account_storage(&self.fingerprint);
+        let u256 = ulm::get_account_storage(self.api, &self.fingerprint);
         match u256.try_into() {
             Ok(v) => v,
-            Err(reason) => fail(reason), 
+            Err(reason) => fail(reason),
         }
     }
 }
 
-pub struct SingleChunkStorageBuilder<ValueType:Into<U256> + TryFrom<U256> + 'static> {
-    phantom: PhantomData<ValueType>,
+pub struct SingleChunkStorageBuilder<'a, ValueType>
+    where
+        ValueType: Into<U256> + TryFrom<U256>,
+{
+    phantom_value: PhantomData<&'a ValueType>,
+    api: &'a mut dyn ulm::Ulm,
     encoder: Encoder,
 }
 
-impl<ValueType:Into<U256> + TryFrom<U256, Error = &'static str>> SingleChunkStorageBuilder<ValueType> {
-    pub fn new(name: &String) -> Self {
+impl<'a, ValueType> SingleChunkStorageBuilder<'a, ValueType>
+    where
+        ValueType: Into<U256> + TryFrom<U256, Error = &'static str>,
+{
+    pub fn new(api: &'a mut dyn ulm::Ulm, name: &String) -> Self {
         let mut encoder = Encoder::new();
         encoder.add(name);
-        Self::from_encoder(encoder)
+        Self::from_encoder(api, encoder)
     }
 
-    fn from_encoder(encoder: Encoder) -> Self {
+    fn from_encoder(api: &'a mut dyn ulm::Ulm, encoder: Encoder) -> Self {
         SingleChunkStorageBuilder::<ValueType> {
-            phantom: PhantomData,
-            encoder
+            phantom_value: PhantomData,
+            api,
+            encoder,
         }
     }
 
@@ -82,9 +97,9 @@ impl<ValueType:Into<U256> + TryFrom<U256, Error = &'static str>> SingleChunkStor
         self.encoder.add(arg);
     }
 
-    pub fn build(&self) -> SingleChunkStorage<ValueType> {
+    pub fn build(&mut self) -> SingleChunkStorage<ValueType> {
         let bytes = self.encoder.encode();
         let fingerprint = ulm_hooks::keccak_hash_int(&bytes);
-        SingleChunkStorage::new(fingerprint)
+        SingleChunkStorage::new(self.api, fingerprint)
     }
 }
--- tests/ulm/erc20/rust/src/ulm.rs
+++ tests/ulm/erc20/rust/src/ulm.rs
@@ -10,28 +10,81 @@ extern "C" {
     pub fn SetAccountStorage(key: *const u8, value: *const u8);
 }
 
-pub fn get_account_storage_helper(key: &[u8; 32], value: &mut [u8; 32]) {
-    unsafe { GetAccountStorage(key.as_ptr(), value.as_mut_ptr()); }
+pub trait Ulm {
+    fn get_account_storage(&self, key: &[u8; 32], value: &mut [u8; 32]);
+    fn set_account_storage(&mut self, key: &[u8; 32], value: &[u8; 32]);
 }
-pub fn get_account_storage(key: &U256) -> U256 {
+
+#[cfg(not(test))]
+struct UlmImpl {}
+
+#[cfg(not(test))]
+impl Ulm for UlmImpl {
+    fn get_account_storage(&self, key: &[u8; 32], value: &mut [u8; 32]) {
+        unsafe { GetAccountStorage(key.as_ptr(), value.as_mut_ptr()); }
+    }
+
+    fn set_account_storage(&mut self, key: &[u8; 32], value: &[u8; 32]) {
+        unsafe { SetAccountStorage(key.as_ptr(), value.as_ptr()); }
+    }
+}
+
+#[cfg(test)]
+mod ulm_mock {
+    use bytes::{Bytes, Buf};
+    use std::collections::HashMap;
+
+    use crate::assertions::fail;
+    use crate::require;
+    use crate::ulm::Ulm;
+
+    pub struct UlmMock {
+        storage: HashMap<Bytes, Bytes>,
+    }
+
+    impl UlmMock {
+        pub fn new() -> UlmMock {
+            UlmMock { storage: HashMap::new() }
+        }
+    }
+
+    impl Ulm for UlmMock {
+        fn get_account_storage(&self, key: &[u8; 32], value: &mut [u8; 32]) {
+            let bytes_key = Bytes::copy_from_slice(key);
+            match self.storage.get(&bytes_key) {
+                Some(v) => {
+                    let bytes_value = v.chunk();
+                    require!(bytes_value.len() == 32, "unexpected value length in storage");
+                    value.copy_from_slice(bytes_value);
+                },
+                None => fail("Key not found in storage"),
+            }
+        }
+
+        fn set_account_storage(&mut self, key: &[u8; 32], value: &[u8; 32]) {
+            let bytes_key = Bytes::copy_from_slice(key);
+            let bytes_value = Bytes::copy_from_slice(value);
+            self.storage.insert(bytes_key, bytes_value);
+        }
+    }
+}
+
+pub fn get_account_storage(api: &dyn Ulm, key: &U256) -> U256 {
     let mut key_bytes = [0_u8; 32];
     key.copy_to_array_le(&mut key_bytes);
 
     let mut value_bytes = [0_u8; 32];
-    get_account_storage_helper(&key_bytes, &mut value_bytes);
+    api.get_account_storage(&key_bytes, &mut value_bytes);
 
     U256::from_array_le(value_bytes)
 }
 
-pub fn set_account_storage_helper(key: &[u8; 32], value: &[u8; 32]) {
-    unsafe { SetAccountStorage(key.as_ptr(), value.as_ptr()); }
-}
-pub fn set_account_storage(key: &U256, value: &U256) {
+pub fn set_account_storage(api: &mut dyn Ulm, key: &U256, value: &U256) {
     let mut key_bytes = [0_u8; 32];
     key.copy_to_array_le(&mut key_bytes);
 
     let mut value_bytes = [0_u8; 32];
     value.copy_to_array_le(&mut value_bytes);
 
-    set_account_storage_helper(&key_bytes, &value_bytes);
+    api.set_account_storage(&key_bytes, &value_bytes);
 }
