cad82d8cc18e5cd68e7f646c4d539d0eeffed733
Virgil <virgil.serbanuta@runtimeverification.com>
2024-12-10 16:30:49 +0200

    Put the contract in a struct

--- tests/ulm/erc20/rust/src/erc20.rs
+++ tests/ulm/erc20/rust/src/erc20.rs
@@ -7,43 +7,56 @@ use crate::encoder::Encoder;
 use crate::storage::{SingleChunkStorage, SingleChunkStorageBuilder};
 use crate::ulm::{log3, Ulm};
 
-fn s_total_supply<'a>(api: Rc<RefCell<dyn Ulm>>) -> SingleChunkStorage<'a, Balance> {
-    SingleChunkStorageBuilder::new(api, &("total_supply".to_string())).build()
+struct Erc20 {
+  api: Rc<RefCell<dyn Ulm>>
 }
 
-fn s_balances<'a>(api: Rc<RefCell<dyn Ulm>>, address: &Address) -> SingleChunkStorage<'a, Balance> {
-    let mut builder = SingleChunkStorageBuilder::new(api, &("balances".to_string()));
-    builder.add_arg(address);
-    builder.build()
-}
+impl Erc20 {
+    pub fn new(api: Rc<RefCell<dyn Ulm>>) -> Self {
+        Erc20 { api }
+    }
 
-fn s_allowances<'a>(api: Rc<RefCell<dyn Ulm>>, account: &Address, spender: &Address) -> SingleChunkStorage<'a, Balance> {
-    let mut builder = SingleChunkStorageBuilder::new(api, &("allowances".to_string()));
-    builder.add_arg(account);
-    builder.add_arg(spender);
-    builder.build()
-}
+    // ---------------------------
 
-// ---------------------------
+    fn s_total_supply<'a>(&self) -> SingleChunkStorage<'a, Balance> {
+        SingleChunkStorageBuilder::new(self.api.clone(), &("total_supply".to_string())).build()
+    }
 
-fn transfer_event(api: &dyn Ulm, from: Address, to: Address, value: &Balance) {
-    let mut encoder = Encoder::new();
-    encoder.add(value);
-    log3(
-        api,
-        "Transfer(address,address,u256)",
-        &from.into(), &to.into(),
-        encoder.encode()
-    )
-}
+    fn s_balances<'a>(&self, address: &Address) -> SingleChunkStorage<'a, Balance> {
+        let mut builder = SingleChunkStorageBuilder::new(self.api.clone(), &("balances".to_string()));
+        builder.add_arg(address);
+        builder.build()
+    }
 
-fn approval_event(api: &dyn Ulm, owner: Address, spender: Address, value: &Balance) {
-    let mut encoder = Encoder::new();
-    encoder.add(value);
-    log3(
-        api,
-        "Approval(address,address,u256)",
-        &owner.into(), &spender.into(),
-        encoder.encode()
-    )
+    fn s_allowances<'a>(&self, account: &Address, spender: &Address) -> SingleChunkStorage<'a, Balance> {
+        let mut builder = SingleChunkStorageBuilder::new(self.api.clone(), &("allowances".to_string()));
+        builder.add_arg(account);
+        builder.add_arg(spender);
+        builder.build()
+    }
+
+    // ---------------------------
+
+    fn transfer_event(&self, from: Address, to: Address, value: &Balance) {
+        let mut encoder = Encoder::new();
+        encoder.add(value);
+        log3(
+            &*self.api.borrow(),
+            "Transfer(address,address,u256)",
+            &from.into(), &to.into(),
+            encoder.encode()
+        )
+    }
+
+    fn approval_event(&self, owner: Address, spender: Address, value: &Balance) {
+        let mut encoder = Encoder::new();
+        encoder.add(value);
+        log3(
+            &*self.api.borrow(),
+            "Approval(address,address,u256)",
+            &owner.into(), &spender.into(),
+            encoder.encode()
+        )
+    }
+
 }
